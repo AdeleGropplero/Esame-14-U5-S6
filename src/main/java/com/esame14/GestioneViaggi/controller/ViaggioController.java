f57aec69a420c6c74aa97b78ff00b368d5b48936
AdeleGropplero <adele.gdt@hotmail.com>
2025-02-15 16:10:39 +0100

    completamento service quasi ultimato.

--- src/main/java/com/esame14/GestioneViaggi/controller/ViaggioController.java
+++ src/main/java/com/esame14/GestioneViaggi/controller/ViaggioController.java
@@ -3,16 +3,15 @@ package com.esame14.GestioneViaggi.controller;
 import com.esame14.GestioneViaggi.Service.ViaggioService;
 import com.esame14.GestioneViaggi.payload.ViaggioDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -38,12 +37,13 @@ public class ViaggioController {
         }
     }
 
+    // POSTMAN --> http://localhost:8080/viaggi/multipli
     @PostMapping("/multipli")
     public ResponseEntity<String> createViaggiMultipli(@RequestBody @Validated List<ViaggioDTO> viaggiDTO, BindingResult validation) {
         if (validation.hasErrors()) {
             String messaggioErrore = "ERRORI DI VALIDAZIONE \n";
             for (ObjectError error : validation.getAllErrors()) {
-                messaggioErrore += error.getDefaultMessage() + ", \n";
+                messaggioErrore += error.getDefaultMessage() + ",\n";
             }
             return new ResponseEntity<>(messaggioErrore, HttpStatus.NOT_ACCEPTABLE);
         } else {
@@ -52,18 +52,38 @@ public class ViaggioController {
         }
     }
 
-    @
+    // POSTMAN --> http://localhost:8080/viaggi
+    @GetMapping
+    public ResponseEntity<Page<ViaggioDTO>> getAllViaggi(Pageable pageable) {
+        return ResponseEntity.ok(viaggioService.getAllViaggi(pageable));
+    }
 
+    // POSTMAN --> http://localhost:8080/viaggi/id
+    @GetMapping("/{id}")
+    public ResponseEntity<ViaggioDTO> getViaggioById(@PathVariable Long id) {
+        return ResponseEntity.ok(viaggioService.getViaggioById(id));
+    }
 
+    // POSTMAN --> http://localhost:8080/viaggi/update/id
+    @PutMapping("/update/{id}")
+    public ResponseEntity<String> updateViaggio(@PathVariable Long id, @RequestBody @Validated ViaggioDTO viaggioDTO, BindingResult validation) {
+        if (validation.hasErrors()) {
+            String messaggioErrore = "ERRORI DI VALIDAZIONE \n";
+            for (ObjectError error : validation.getAllErrors()) {
+                messaggioErrore += error.getDefaultMessage() + ", \n";
+            }
+            return new ResponseEntity<>(messaggioErrore, HttpStatus.NOT_ACCEPTABLE);
+        } else {
+            viaggioService.patchViaggio( id, viaggioDTO);
+            return new ResponseEntity<>("Il post è stato modificato correttamente", HttpStatus.OK);
+        }
+    }
 
-
-
-
-
-
-
-
-
+    // POSTMAN --> http://localhost:8080/viaggi/delete/id
+    @DeleteMapping("/delete/{id}")
+    public String cancellaViaggio(@PathVariable Long id) {
+        return viaggioService.deleteViaggio(id);
+    }
 
 
 }
